f8ac8569e4ae3a1db1371c5673c30fd52626d692
Lemontea <lemontea.Tom@gmail.com>
2024-04-03 03:11:02 +0800

    save change

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,49 @@
 import { read } from 'fs';
 import * as vscode from 'vscode';
 
+import { IVSCodeExtLogger, getExtensionLogger } from '@vscode-logging/logger';
+
+
+async function showQuickPick() {
+	let i = 0;
+	const result = await vscode.window.showQuickPick(["Parcel", "create-react-app", "others..."], {
+		placeHolder: 'unknown',
+		onDidSelectItem: item => vscode.window.showInformationMessage(`Focus ${++i}: ${item}`),
+		title: 'Choose your Tech stack'
+	});
+	vscode.window.showInformationMessage(`Selected: ${result}`);
+	return result;
+}
+
+class LLMProvider {
+	//private _baseurl: string, private _accessToken: string, 
+	constructor(private readonly _logger: IVSCodeExtLogger) {}
+
+	async chatcompletion(my_messages: any, modelid : string) {
+		const apiUrl = vscode.workspace.getConfiguration('juniordev').get('conf.provider.openai.baseUrl') + "/v1/chat/completions";
+		const reqBody = {
+			model: modelid,
+			messages: my_messages,
+			max_tokens: 4000,
+			stream: false
+		}
+		const opt = {
+			method: 'POST',
+			headers: {Authorization: 'Bearer ' + vscode.workspace.getConfiguration('juniordev').get('conf.provider.accessToken'), 'Content-Type': 'application/json'},
+			body: JSON.stringify(reqBody)
+		}
+		/*fetch(apiUrl, opt)
+		  .then(response => response.json())
+		  .then(response => { 
+			this._logger.info(JSON.stringify(response));
+			})
+		  .catch(err => console.error(err))*/
+		const result = await fetch(apiUrl, opt);
+		const j: any = await result.json();
+		this._logger.info(JSON.stringify(j));
+		return j.choices[0].message.content;
+	}
+}
 
 class CodingAgentViewProvider implements vscode.WebviewViewProvider {
 	public static readonly viewType = "juniordev.codingAgentSidePanel";
@@ -10,7 +53,8 @@ class CodingAgentViewProvider implements vscode.WebviewViewProvider {
 	private _view?: vscode.WebviewView;
 
 	constructor(
-		private readonly _extensionUri: vscode.Uri
+		private readonly _extensionUri: vscode.Uri,
+		private readonly _logger : IVSCodeExtLogger
 	) {}
 
 	my_updateWebviewView() {
@@ -32,11 +76,42 @@ class CodingAgentViewProvider implements vscode.WebviewViewProvider {
 
 		webviewView.webview.html = this._getHtmlForWebView(webviewView.webview);
 
+		const sys_prompt = 
+`You are a senior frontend engineer. Our project is setup with these tech stack:
+- React app scaffolded with \`create-react-app\`
+- Material UI chosen as the component UI library, installed with react integration using this command:
+\`npm install @mui/material @emotion/react @emotion/styled @mui/icons-material\`
+  - Notice that \`emotion\` is chosen as the styling engine/CSS-in-JS solution.
+  - Material Icons font is installed, so you can start using the \`Icon\` component right away.
+- \`axios\` is installed so that calling backend API is easier
+- Directory: \`src/pages\` for the pages, \`src/components\` for sub-page level components.
+
+Your task is to fulfill user's request by implementing their requirement in the context of the project setup above. Show your code.`;
+
 		webviewView.webview.onDidReceiveMessage(data => {
 			switch (data.type) {
+				case 'logData':
+					console.log(data.value);
+					this._logger.info(JSON.stringify(data.value));
+					break;
 				case 'startAgent':
 					vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(`Pasted: ${data.value}`))
-					vscode.window.activeTerminal?.sendText('python3 -m venv venv; source venv/bin/activate', true);
+					//vscode.window.activeTerminal?.sendText('python3 -m venv venv; source venv/bin/activate', true);
+					const llm = new LLMProvider(this._logger);
+					llm.chatcompletion([{
+						role: 'system',
+						content: sys_prompt
+					}, {
+						role: 'user',
+						content: `Design and implement this: ${data.value}. Let's think step by step:
+- Begin with the overall page layout. Assume that we've decomposed into our custom UI components already, and use those components in your layout.
+- Then, implement the custom UI components you've identified in the previous step.
+- As we're in prototype phase, assume we're using a mock backend API (you're free to chose the req/res schema and endpoint URL).
+
+Show all of your codes.`
+					}], 'mistral-7b-instruct').then(s => {
+						vscode.window.activeTextEditor?.insertSnippet(new vscode.SnippetString(s));
+					});
 					break;
 				default:
 					vscode.window.showInformationMessage('unknown')
@@ -93,7 +168,9 @@ class CodingAgentViewProvider implements vscode.WebviewViewProvider {
 				}
 				fetch(apiUrl, opt)
 				  .then(response => response.json())
-				  .then(response => { console.log(response); output.innerText = response.choices[0].message.content; })
+				  .then(response => { 
+					vscode.postMessage({ type: 'logData', value: response });
+					output.innerText = response.choices[0].message.content; })
 				  .catch(err => console.error(err))
 			  }
 
@@ -115,7 +192,14 @@ class CodingAgentViewProvider implements vscode.WebviewViewProvider {
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	const provider = new CodingAgentViewProvider(context.extensionUri);
+	const extLogger = getExtensionLogger({
+		extName: "JuniorDev",
+		level: "info",
+		logPath: context.logPath,
+		logConsole: true
+	});
+	extLogger.info("Hey man there - this is the beginning.");
+	const provider = new CodingAgentViewProvider(context.extensionUri, extLogger);
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -124,7 +208,7 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('juniordev.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('juniordev.helloWorld', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from juniordev!');
@@ -133,6 +217,10 @@ export function activate(context: vscode.ExtensionContext) {
 		//vscode.window.showInformationMessage('BaseURL: ')
 		console.log(vscode.workspace.getConfiguration('juniordev').get('conf.provider.openai.baseUrl'));
 		provider.my_updateWebviewView();
+
+		//const quickPick = vscode.window.createQuickPick()
+		const r1 = await showQuickPick();
+		console.log(r1);
 	});
 
 	context.subscriptions.push(disposable);
